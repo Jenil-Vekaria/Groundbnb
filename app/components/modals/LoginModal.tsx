8d7a3c283a266b54a858ce7b299d0f2c232b014e
Jenil-Vekaria <jenilvekaria@outlook.com>
2023-04-26 22:07:23 -0400

    add toggle between register and login modal

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { AiFillGithub } from "react-icons/ai";
 import { FcGoogle } from "react-icons/fc";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
@@ -52,6 +52,11 @@ const LoginModal = () => {
 		});
 	};
 
+	const toggle = useCallback(() => {
+		loginModal.onClose();
+		registerModal.onOpen();
+	}, [loginModal, registerModal]);
+
 	const bodyContent = (
 		<div className="flex flex-col gap-4">
 			<Heading title="Welcome back" subtitle="Login to your account" />
@@ -94,12 +99,12 @@ const LoginModal = () => {
 
 			<div className="text-neutral-500 mt-4 font-light">
 				<div className="flex flex-row justify-center items-center gap-2">
-					<div>Already have an account?</div>
+					<div>First time using Groundbnb?</div>
 					<div
-						onClick={loginModal.onClose}
+						onClick={toggle}
 						className="text-neutral-800 cursor-pointer hover:underline"
 					>
-						Log in
+						Sign up
 					</div>
 				</div>
 			</div>

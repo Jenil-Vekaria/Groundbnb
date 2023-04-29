70369f7a696fe5b3d7ce64e7d7df0f53ce9f7663
Jenil-Vekaria <jenilvekaria@outlook.com>
2023-04-29 00:31:57 -0400

    Add API route to create listing

---
+++ app/api/listings/route.ts
@@ -0,0 +1,47 @@
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import prisma from "@/app/libs/prismadb";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+	const currentUser = await getCurrentUser();
+
+	if (!currentUser) {
+		return NextResponse.error();
+	}
+
+	const body = await req.json();
+	const {
+		category,
+		location,
+		guestCount,
+		roomCount,
+		bathroomCount,
+		imageSrc,
+		price,
+		title,
+		description,
+	} = body;
+
+	Object.keys(body).forEach((value: any) => {
+		if (!body[value]) {
+			return NextResponse.error();
+		}
+	});
+
+	const listing = await prisma.listing.create({
+		data: {
+			title,
+			description,
+			imageSrc,
+			category,
+			roomCount,
+			bathroomCount,
+			guestCount,
+			locationValue: location.value,
+			price: parseInt(price, 10),
+			userId: currentUser.id,
+		},
+	});
+
+	return NextResponse.json(listing);
+}

b987a58b3f7f7a4c306c46da237d40fc8180761c
Jenil-Vekaria <jenilvekaria@outlook.com>
2023-05-09 16:22:36 -0400

    Add UI loader

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import LoginModal from "./components/modals/LoginModal";
 import getCurrentUser from "./actions/getCurrentUser";
 import RentModal from "./components/modals/RentModal";
 import SearchModal from "./components/modals/SearchModal";
+import ClientOnly from "./components/ClientOnly";
 
 export const metadata = {
 	title: "Groundbnb",
@@ -27,12 +28,14 @@ export default async function RootLayout({
 	return (
 		<html lang="en">
 			<body className={`${font.className}`}>
-				<ToastProvider />
-				<RegisterModal />
-				<LoginModal />
-				<RentModal />
-				<SearchModal />
-				<Navbar currentUser={currentUser} />
+				<ClientOnly>
+					<ToastProvider />
+					<RegisterModal />
+					<LoginModal />
+					<RentModal />
+					<SearchModal />
+					<Navbar currentUser={currentUser} />
+				</ClientOnly>
 				<div className="pb-20 pt-28">{children}</div>
 			</body>
 		</html>
